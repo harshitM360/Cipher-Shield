59703424b7a8c644758220ac300381acd257ad0b
harshitM360 <135355659+harshitM360@users.noreply.github.com>
2026-04-16 16:27:59 +0530

    Add files via upload

---
+++ agent/__init__.py
@@ -0,0 +1,15 @@
+from .summarizer import (
+    SocReport,
+    SocRuleSummary,
+    SocTechniqueSummary,
+    SocReportGenerator,
+    generate_all_soc_reports,
+)
+
+__all__ = [
+    'SocReport',
+    'SocRuleSummary',
+    'SocTechniqueSummary',
+    'SocReportGenerator',
+    'generate_all_soc_reports',
+]
